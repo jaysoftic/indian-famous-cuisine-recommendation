cd9680d8cff3fa0c5f11f20b3641c352629b0edb
Jay Soni <49163967+jaysoftic@users.noreply.github.com>
2021-03-04 16:58:06 +0530

    v1

--- automate_recipe_videos_link.py
+++ automate_recipe_videos_link.py
@@ -5,18 +5,24 @@ import pandas as pd
 
 data = pd.read_csv("artifacts/cleaned_data.csv") # here i read csv file of cleaned data
 
-data["recipe"] = "empty" # here i created a recipe column with empty string
+if "recipe" not in data.columns:
+    data["recipe"] = "empty" # here i created a recipe column with empty string
 
-for i in range(data.shape[0]): # here is the for loop iterate through size of rows
-    dish = data.loc[i, "name"]   # here i get a name of dish base on index from loop
+try:
+    for i in range(data.shape[0]): # here is the for loop iterate through size of rows
 
-    # below i'm getting unique video id using keyword search which will be use later on for showing video on web app
-    search = SearchVideos(dish + " indian recipe in english", offset = 1, mode = "dict", max_results = 1)
-    print(i, dish) # here i print dish for confirmation
+        if data.loc[i, "recipe"] == "empty": # if there is unique id available then it's not going to run again
+            dish = data.loc[i, "name"]   # here i get a name of dish base on index from loop
 
-    # below i store a recipe video unique id to appropriate index
-    data.loc[i, "recipe"] = search.result()["search_result"][0]["link"].split("=")[1]
+            # below i'm getting unique video id using keyword search which will be use later on for showing video on web app
+            search = SearchVideos(dish + " indian recipe in english", offset = 1, mode = "dict", max_results = 1)
+            print(i, dish) # here i print dish for confirmation
 
-# here again save a csv file which contain all data and recipe's unique youtube id
-data.to_csv("cleaned_data.csv", index = False)
+            # below i store a recipe video unique id to appropriate index
+            data.loc[i, "recipe"] = search.result()["search_result"][0]["link"].split("=")[1]
 
+            # here again save a csv file which contain all data and recipe's unique youtube id
+            data.to_csv("artifacts/cleaned_data.csv", index = False)
+except:
+    print("----------------------------")
+    print("API limit exceed try to run again.")
